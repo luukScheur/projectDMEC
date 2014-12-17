c75bba49b8379fcb66138b2efc36efddf6548f6b
LuukScheur <luukvdscheur1@hotmail.com>
2014-12-17 10:19:24 +0100

    materiaalOverzicht

--- client/js/routes.js
+++ client/js/routes.js
@@ -1,6 +1,21 @@
 "use strict";
 var MOOCBOOKApp = angular.module("MOOCBOOKApp", ['ngRoute', 'ngResource']);
 
+// Custom filter for materiaalOverzicht (Controller) page
+MOOCBOOKApp.filter('filterTypes', function() {
+    return function(list, selectedTypes) {
+				return list.filter(function(item) {
+					if(selectedTypes[0] == null) {
+						return true;
+					} else {
+						if (selectedTypes.indexOf(item.type) != -1) {
+							return true;
+						}
+						return false;
+					}
+				});
+    };
+})
 /*
 MOOCBOOKApp.factory('socket', function() {
 	var socket = io.connect('http://localhost:3000');
@@ -9,11 +24,15 @@ MOOCBOOKApp.factory('socket', function() {
 */
 
 MOOCBOOKApp.config(['$routeProvider', function ($routeProvider) {
-	$routeProvider.when('/docent', {
-		templateUrl: './views/docent.html',
-		controller: docentController
+	$routeProvider.when('/home', {
+		templateUrl: './views/homeIngelogd.html',
+		controller: homeIngelogdController
+	});
+	$routeProvider.when('/materiaalOverzicht', {
+		templateUrl: './views/materiaalOverzicht.html',
+		controller: materiaalOverzichtController
 	});
 	$routeProvider.otherwise({
-		redirectTo: "/docent"
+		redirectTo: "/home"
 	});
 }]);

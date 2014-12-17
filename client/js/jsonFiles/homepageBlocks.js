e59406df444d20e64e501028f312ea611ff28f7f
LuukScheur <luukvdscheur1@hotmail.com>
2014-12-17 10:20:53 +0100

    MateriaalOverzicht

---
+++ client/js/jsonFiles/homepageBlocks.js
@@ -0,0 +1,51 @@
+var homepageBlocks = [
+	{
+		id: 0,
+		title: 'Gewijzigd materiaal',
+		description: 'en hier de tekst '
+	},
+	{
+		id: 1,
+		title: 'Nieuw materiaal',
+		description: 'en hier de tekst'
+	},
+	{
+		id: 2,
+		title: 'Meest bekeken les',
+		description: 'en hier de tekst'
+	},
+	{
+		id: 3,
+		title: 'Nieuws',
+		description: 'en hier de tekst'
+	},
+	{
+		id: 4,
+		title: 'Wat wordt er nu bekeken?',
+		description: 'en hier de tekst'
+	},
+	{
+		id: 5,
+		title: 'Tagscloud',
+		description: 'en hier de tekst'
+	},
+	{
+		id: 6,
+		title: 'Algemene discussie',
+		description: 'en hier de tekst'
+	},
+	{
+		id: 7,
+		title: 'Gevolgde discussie',
+		description: 'en hier de tekst'
+	},
+	{
+		id: 8,
+		title: 'Meest bekeken materiaal',
+		description: 'en hier de tekst'
+	},
+	{
+		id: 9,
+		title: 'Nieuwe lessen',
+		description: 'en hier de tekst'
+	}];
